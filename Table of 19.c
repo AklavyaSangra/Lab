234c0d92ba2968d5e438ee9757610d553bed1201
AklavyaSangra <146859465+AklavyaSangra@users.noreply.github.com>
2024-01-06 01:22:35 +0530

    Update and rename Table of 19.md to Table of 19.c

--- Table of 19.c
+++ Table of 19.c
@@ -10,7 +10,3 @@ Write a c program to print the table of 19.
     }
 
 
-Output of the program:
-
-
-![image](https://github.com/AklavyaSangra/Lab/assets/146859465/3fdee12e-ba08-428b-8d83-fe8dd65136cf)
