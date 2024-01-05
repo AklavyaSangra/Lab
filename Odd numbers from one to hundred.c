59953be27e0a4333f15454db3eef17d7b471c656
AklavyaSangra <146859465+AklavyaSangra@users.noreply.github.com>
2024-01-06 01:15:19 +0530

    Update and rename Odd numbers from one to hundred.md to Odd numbers from one to hundred.c

--- Odd numbers from one to hundred.c
+++ Odd numbers from one to hundred.c
@@ -13,7 +13,4 @@ Write a c program to print odd numbers from 1 to 100.
     }
 
 
-Output of program:
 
-
-![image](https://github.com/AklavyaSangra/Lab/assets/146859465/2ddd54db-e218-4b8b-821e-91864123dae1)
