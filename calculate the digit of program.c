a988e141a1cf41a7c00a81fa97665267af13c27b
AklavyaSangra <146859465+AklavyaSangra@users.noreply.github.com>
2024-01-06 01:27:05 +0530

    Update and rename calculate the digit of program.md to calculate the digit of program.c

--- calculate the digit of program.c
+++ calculate the digit of program.c
@@ -17,7 +17,4 @@ Write the c program to calculate the no of digit of the program.
     }
 
 
-    Output of the program:
-
-
-![image](https://github.com/AklavyaSangra/Lab/assets/146859465/543f89fc-9c9c-4ce3-a301-833e5a29465a)
+    
