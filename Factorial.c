864f236ac65acda939ad48415a08476c14e6d605
AklavyaSangra <146859465+AklavyaSangra@users.noreply.github.com>
2024-01-06 01:12:12 +0530

    Update and rename Factorial.md to Factorial.c

--- Factorial.c
+++ Factorial.c
@@ -14,8 +14,3 @@ Write a c program to calculate the factorial of numbers.
     printf("the factorial of number is:%d",product);
     }
 
-
-Output of the program:
-
-
-![image](https://github.com/AklavyaSangra/Lab/assets/146859465/e12f2115-583b-442c-ad14-865076ab956e)
