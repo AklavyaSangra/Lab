dfc665becce3f06df4b34d69efb2e9aabfd7449f
AklavyaSangra <146859465+AklavyaSangra@users.noreply.github.com>
2024-01-06 01:46:26 +0530

    Create ifelse.c

---
+++ ifelse.c
@@ -0,0 +1,25 @@
+Write a c code to check whether a number is less than greater than or equal to 100.
+
+  
+  #include<stdio.h>
+
+int main()
+{
+
+    int number;
+    printf("Please enter a number:\n");
+    scanf("%d",&number);
+    /*
+        For single statements we can skip the curly brackets
+    */
+    if(number < 100)
+        printf("Number is less than 100!\n");
+    else if(number == 100)
+        printf("Number is 100!\n");
+    else
+        printf("Number is greater than 100!\n");
+
+
+    
+    return 0;
+}
