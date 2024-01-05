b71356162fd10cb2ff1e53f2559d5279a6be244a
AklavyaSangra <146859465+AklavyaSangra@users.noreply.github.com>
2024-01-06 01:08:25 +0530

    Update and rename Absolute number.md to Absolute number.c

--- Absolute number.c
+++ Absolute number.c
@@ -19,9 +19,6 @@ Write a c code to check wheter a number is absolute or not.
     }
 
 
-Output of the program:
 
-
-![image](https://github.com/AklavyaSangra/Lab/assets/146859465/43f11afc-ad8e-478c-870e-7ad8e11b1f20)
 
     
